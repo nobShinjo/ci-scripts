bb7644b9c5932d266660d9615a17d217020175ed
Nob Shinjo <90700567+nobShinjo@users.noreply.github.com>
2025-05-05 23:21:15 +0900

    fix(ci): adjust branch iteration in sync-editorconfig job to exclude main branch

--- .gitlab/ci/scripts/coding-style/copy_to_branch.sh
+++ .gitlab/ci/scripts/coding-style/copy_to_branch.sh
@@ -22,10 +22,11 @@ for file in ${FILES}; do
     cp "${file}" .
     git add "$(basename "$file")"
 done
-if git diff --staged --quiet; then
-    echo "ℹ️ No changes to commit for branch ${BRANCH_NAME}, skipping..."
-else
+git status
+if ! git diff --staged --quiet; then
     echo "✏️ Committing changes to ${BRANCH_NAME}..."
     git commit -m "chore(coding-style): Update coding style files [ci skip]"
     git push origin "${BRANCH_NAME}"
+else
+    echo "ℹ️ No changes to commit for branch ${BRANCH_NAME}, skipping..."
 fi
